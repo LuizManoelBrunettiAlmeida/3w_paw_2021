8c40857a72fc7859765c2f50071e5c9c7b28c472
LuizManoelBrunettiAlmeida <la4590180@gmail.com>
2021-08-18 18:14:16 -0300

    Continuação do projeto de paw

--- ion3bim1/src/app/home/home.page.ts
+++ ion3bim1/src/app/home/home.page.ts
@@ -6,6 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  login = '';
+  senha = '';
+  mensagem = '';
+
+  validar(): void{
+    if(this.login === 'admin' && this.senha === '1234'){
+      this.mensagem = 'Logado com sucesso!'
+    }else{
+      this.mensagem = 'Login ou Senha incorretos!'
+    }
+  }
 
   constructor() {}
 
